0d58c90195fd438b97bb58d6475173bc9b9a9ce6
houdaoussar <bi52ew@student.sunderland.ac.uk>
2025-04-08 16:48:57 +0100

    Second commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import home, google_sheets, file_upload, results
+import home, google_sheets, file_upload, results, llm_summarization
 
 st.set_page_config(page_title="Amazon Sentiment Analysis System")
 
 st.title("Amazon Review Sentiment Analysis System")
-choice = st.sidebar.selectbox("My Menu", ("Home", "Analysis Via Google Sheets", "Analysis Via File Upload", "Results"))
+choice = st.sidebar.selectbox("My Menu", ("Home", "Analysis Via Google Sheets", "Analysis Via File Upload", "Results", "AI Summarization"))
 
 # Route to the appropriate page based on user selection
 if choice == "Home":
@@ -14,5 +14,6 @@ elif choice == "Analysis Via Google Sheets":
 elif choice == "Analysis Via File Upload":
     file_upload.analyze_file_upload()
 elif choice == "Results":
-    results.show_results() 
-
+    results.show_results()
+elif choice == "AI Summarization":
+    llm_summarization.show_summarization()
